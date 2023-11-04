e3967ea26039506a3afdaed3138b013f42f2d8c5
akin <braingrams40@gmail.com>
2023-11-04 01:25:29 +0100

    updated footer link

--- src/lib/layout/Footer.tsx
+++ src/lib/layout/Footer.tsx
@@ -5,7 +5,11 @@ const Footer = () => {
     <Flex as="footer" width="full" justifyContent="center">
       <Text fontSize="sm">
         {new Date().getFullYear()} -{' '}
-        <Link href="https://sznm.dev" isExternal rel="noopener noreferrer">
+        <Link
+          href="https://bamfolio.netlify.app"
+          isExternal
+          rel="noopener noreferrer"
+        >
           Developed with ♡ by Brain for PAB class XXIII
         </Link>
       </Text>
